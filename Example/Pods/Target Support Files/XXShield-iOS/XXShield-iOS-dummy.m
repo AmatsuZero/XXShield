87af040546b147ed231447b50c7957c98cf4353a
Daubert <jzh16s@hotmail.com>
2017-12-06 11:39:19 +0800

    ### add module map for build

---
+++ Example/Pods/Target Support Files/XXShield-iOS/XXShield-iOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_XXShield_iOS : NSObject
+@end
+@implementation PodsDummy_XXShield_iOS
+@end
---
+++ Example/Pods/Target Support Files/XXShield-macOS/XXShield-macOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_XXShield_macOS : NSObject
+@end
+@implementation PodsDummy_XXShield_macOS
+@end
